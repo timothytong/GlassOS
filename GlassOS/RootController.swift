f708e3a8037d69ce9bc984bf609676cacf80cfde
timothytong <timothy.tong@hotmail.ca>
2015-01-04 20:01:40 -0500

    GPUImage needs to be reimported.

--- GlassOS/RootController.swift
+++ GlassOS/RootController.swift
@@ -97,7 +97,7 @@ class RootController: UIViewController {
 
         }
     }
-    
+    /*
     func addObservers(){
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "handle:", name: SVProgressHUDWillAppearNotification, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "handle:", name: SVProgressHUDDidAppearNotification, object: nil)
@@ -106,6 +106,7 @@ class RootController: UIViewController {
         
         SVProgressHUD.setFont(UIFont(name: "HelveticaNeue-Thin", size: 20))
     }
+*/
     
     func handle(notif:NSNotification){
         //        NSLog("Notification recieved: \(notif.name)");
